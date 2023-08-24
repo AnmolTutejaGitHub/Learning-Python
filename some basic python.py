580a6f24fb7ea685ba26f95dad1ac67af52d9008
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-08-24 16:35:30 +0530

    Create some basic python.py

---
+++ some basic python.py
@@ -0,0 +1,98 @@
+# python - coding Ninja
+print("Hello")
+a=10
+b=2
+
+sum=a+b
+print(sum)
+
+do=sum+12   
+print(do) 
+print(type(sum))
+
+a="2"+"2"
+print(a)   
+
+name="anmol"
+
+Uname=name.upper()
+print(Uname)
+
+m=10
+n=20
+
+multiple=m*n
+print("multiple")
+print(multiple)
+
+# commented out
+
+### assigning differentb type of data in a variable
+
+a=10
+a=45
+a="ANMOL TUTEJA"
+print(a)
+print(type(a))
+
+var1=10
+print(var1)
+
+var_1=45
+print(var_1)
+
+_var1=23
+print(_var1)
+
+x=10
+x="abcd"
+print(x)
+
+y="abcd"
+y=10
+
+print(type(y))
+
+w=2+4j
+print(type(w))
+
+# a=a+1
+# print(a)
+
+a=12
+print(id(a))
+b=12
+print(id(b))
+
+a=13
+print(id(a))
+
+# a=1000
+# b=1000
+# print(id(a))  #because python does that optimisation only for numbers between -5 to 256
+# print(id(b))
+
+# a=-6
+# b=-6
+# print(id(a))
+# print(id(b))
+
+
+p=10
+print(id(p))
+
+q=10+2-2
+print(id(q))
+
+
+a=100000000000000000000000000034455555555555666
+#print(sizeof(a)) 
+
+#Arithmetic operators
+a=9
+b=45
+print(a+b)
+
+a=9
+b=2
+print(a/b)
