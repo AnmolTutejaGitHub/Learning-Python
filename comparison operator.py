43a889bc42a43b2895059d5d4aa47e8d8d3b9fbe
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-08-24 18:16:22 +0530

    Add files via upload

---
+++ comparison operator.py
@@ -0,0 +1,21 @@
+# comparison operator
+
+A=bool(2==2)
+print(A)
+
+B=bool("hello"=="bye")
+print(B)
+
+C=bool('2'==2)
+print(C)
+S=type(C)
+
+x=2
+y=2.0
+z=bool(x==y) #returning True
+print(z)
+
+
+
+
+
