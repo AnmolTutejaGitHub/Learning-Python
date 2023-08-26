9ad2a7acf6e44b62a3a92a43ed7d0e6ee0849f07
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-08-26 18:27:23 +0530

    Add files via upload

---
+++ in operator.py
@@ -0,0 +1,12 @@
+#in operator
+
+print('x' in [1,2,3])
+print('x' in ['x','y','z'])
+print('a' in 'a world')
+print('a' in 'A world')
+print('mykey' in {'mykey':345})
+
+d={'mykey':345}
+print(345 in d.keys())
+print(345 in d.values())
+
