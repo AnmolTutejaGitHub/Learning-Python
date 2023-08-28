deab7cbf9f48f3204348711bd66f72b92284a06a
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-08-28 23:37:23 +0530

    Add files via upload

---
+++ FizzBuzz Question.py
@@ -0,0 +1,14 @@
+#FizzBuzz Question
+#Write a program that prints the integers from 1 to 100.But for multiples 
+#of three print "Fizz" instead of the number and for the multiple of 5 print "Buzz".
+#For numbers which are multiples of both 3 and 5 print "FizzBuzz"
+
+for num in range(1,101):
+    if num%3==0 and num%5==0:
+        print("FizzBuzz")
+    elif num%3==0:
+        print("Fizz")
+    elif num%5==0:
+        print("Buzz")
+    else :
+        print(num)
