307ea095b59d750ea7216df52ba329643bfa359f
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-08-31 16:55:08 +0530

    Update print unique elements of the list.py

--- print unique elements of the list.py
+++ print unique elements of the list.py
@@ -4,3 +4,14 @@ def unique_list(st):
     return list(set(st))
 
 print(unique_list([1,1,1,1,2,2,2,3,3,3,4,4,5,5,5,66,6,7,6,7,5,6,4,6,4,6,4,56,7,90,0,97,5,6,8,9,6,78]))
+#OR can do this (logical method)
+
+def uniquee_list(a):
+    seen_numbers=[]
+    for number in a:
+        if number not in seen_numbers:
+            seen_numbers.append(number)
+    return seen_numbers
+
+print(uniquee_list([1,1,1,2,3,45,6,6,6,12,2,2,2,6,8,8,8])
+
