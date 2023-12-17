83fe78e4b2b343765da4f31fb589b1a5e234dc8c
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-12-17 22:33:55 +0530

    Create capitalize vs title .py

---
+++ capitalize vs title .py
@@ -0,0 +1,25 @@
+# In Python, title() and capitalize() are both string methods that can be used to modify the capitalization of strings, but they are used in slightly different ways.
+
+# capitalize() method:
+
+# This method capitalizes the first character of a string and makes all other characters in the string lowercase.
+
+# Example:
+
+text = "hello world"
+capitalized_text = text.capitalize()
+print(capitalized_text)
+#Hello world
+
+# title() method:
+
+# This method capitalizes the first character of each word in a string.
+
+# Example:
+
+text = "hello world"
+titled_text = text.title()
+print(titled_text)
+
+# Hello World
+# In summary, capitalize() capitalizes only the first character of the entire string, while title() capitalizes the first character of each word in the string. Choose the method that fits your specific capitalization requirements.
