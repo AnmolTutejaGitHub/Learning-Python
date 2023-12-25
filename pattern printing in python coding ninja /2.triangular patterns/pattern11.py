73175f72a61635b9254bf341a8fa5285598410af
Anmol Tuteja <tutejaanmol54@gmail.com>
2023-12-25 19:21:37 +0530

    Add files via upload

---
+++ pattern printing in python coding ninja /2.triangular patterns/pattern11.py
@@ -0,0 +1,23 @@
+#     *
+#    ***
+#   *****
+#  *******
+# *********
+
+n = int(input())
+
+i = 1
+while i <= n:
+    k = 1
+    while k <= n - i:
+        print(" ", end="")
+        k += 1
+
+    j = 1
+    while j <= 2 * i - 1:
+        print("*", end="")
+        j += 1
+
+    print()
+
+    i += 1
